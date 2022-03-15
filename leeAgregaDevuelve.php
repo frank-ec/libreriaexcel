7c18ffef51db5294bc4769fae8abaa53f8a12ba1
frank-ec <franklynlara@hotmail.com>
2022-03-15 15:38:17 -0500

    Primera subida

--- leeAgregaDevuelve.php
+++ leeAgregaDevuelve.php
@@ -5,7 +5,7 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 ### Conexion a la base de datos
-$mysqli = new mysqli('localhost', 'root', '3201442Ab*', 'emergencia_baseMadre');
+$mysqli = new mysqli('localhost', 'root', '', '');
 mysqli_query($mysqli ,"SET CHARACTER SET 'utf8'");
 mysqli_query($mysqli ,"SET SESSION collation_connection ='utf8_unicode_ci'");
 
